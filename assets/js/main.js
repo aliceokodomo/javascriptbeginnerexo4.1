fe2f901ddbededdce694b97ff98712eb054f984f
AliceOKODOMO <67841123+aliceokodomo@users.noreply.github.com>
2021-04-21 15:58:05 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+function displayArray(){
+var language =["Php","Javascript","C+","TypeScript",];
+var content = document.getElementById('screen');
+var btn = document.getElementById('array');
+alert(language);
+content.innerHTML=language;
+}
